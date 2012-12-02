62654db10d05539e3afe58b43f62ce2f5520ab6a
Wojciech Danilo <wojtek.danilo@gmail.com>
2012-12-02 16:14:07 +0100

    update

--- system/env.zsh
+++ system/env.zsh
@@ -1,1 +1,5 @@
 export EDITOR='vi'
+# fonts antialiasing in java
+export _JAVA_OPTIONS="-Dawt.useSystemAAFontSettings=lcd -Dswing.aatext=true"
+xset r rate 200 50
+setxkbmap pl
--- system/path.zsh
+++ system/path.zsh
@@ -1,1 +1,5 @@
-export PATH="$CONFIG/bin:$PATH"
+export PATH=$CONFIG/bin:/opt/cuda-5.0/bin:$PATH:/opt/pycharm-2.6.3/bin/
+export LD_LIBRARY_PATH=/opt/cuda-5.0/lib64:/lib:$LD_LIBRARY_PATH:/usr/lib64/mpich2/lib
+
+export JAVA_HOME="/opt/jdk1.7.0_09"
+export PATH=${JAVA_HOME}/bin:${PATH}
